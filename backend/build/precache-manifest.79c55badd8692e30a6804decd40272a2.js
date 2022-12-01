04f52f296eb28fc8736ce793d0cb1ad4fa884a87
Mujaddid Al Magribi <almagribi.appdev@gmail.com>
2022-12-01 10:58:48 +0700

    reverse deleted folder

---
+++ backend/build/precache-manifest.79c55badd8692e30a6804decd40272a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fcb725912ad4c27a3f9",
+    "url": "/static/css/main.6461619b.chunk.css"
+  },
+  {
+    "revision": "6fcb725912ad4c27a3f9",
+    "url": "/static/js/main.6fcb7259.chunk.js"
+  },
+  {
+    "revision": "7283b7baec07983e9c40",
+    "url": "/static/css/1.4d3da854.chunk.css"
+  },
+  {
+    "revision": "7283b7baec07983e9c40",
+    "url": "/static/js/1.7283b7ba.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1c322f0bdd2b6c9e4df56020f7dbca5a",
+    "url": "/index.html"
+  }
+];
